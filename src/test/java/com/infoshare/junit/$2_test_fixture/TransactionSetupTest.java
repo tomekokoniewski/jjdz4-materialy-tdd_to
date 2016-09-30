5faa2226ea4a1b9892f37e19fdf2035e06c60342
Michal Ssamujlo <michal.samujlo@gmail.com>
2016-10-01 01:22:01 +0200

    remove unused import

--- src/test/java/com/infoshare/junit/$2_test_fixture/TransactionSetupTest.java
+++ src/test/java/com/infoshare/junit/$2_test_fixture/TransactionSetupTest.java
@@ -24,7 +24,6 @@ public class TransactionSetupTest {
 
     private LoggingActivityMonitor accountActivity;
     private Account account;
-    private ImmutableList<Transaction> transactions;
     private static AccountMonitor activityMonitor;
 
     @BeforeClass
