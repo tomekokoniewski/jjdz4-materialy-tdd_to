99bd413873dc4026fe125fa26d598aa8e4153b3d
Michal Samujlo <michal.samujlo@gmail.com>
2018-01-20 18:09:53 +0100

    add empty test for leap year

---
+++ src/test/java/com/infoshare/junit/LeapYearTest.java
@@ -0,0 +1,10 @@
+package com.infoshare.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+// A leap year is divisible by 4, but is not divisible by 100, unless it is divisible by 400
+public class LeapYearTest {
+}
